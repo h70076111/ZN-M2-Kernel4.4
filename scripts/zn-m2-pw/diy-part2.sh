608e913103ce1c9adfa900b0a40d65fcd510fe8f
OlaveV <heifu303@gmail.com>
2026-04-05 02:04:17 +0800

    Refactor diy-part2.sh for passwall package installation
    Updated the script to install passwall packages and golang.

--- scripts/zn-m2-pw/diy-part2.sh
+++ scripts/zn-m2-pw/diy-part2.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-#优先安装 passwall 源
+
+# 优先安装 passwall 源，并强制覆盖冲突包
 ./scripts/feeds install -a -f -p passwall_packages
 ./scripts/feeds install -a -f -p passwall_luci
+
+# 强制安装我们手动替换的 golang
+./scripts/feeds install -a -f -p packages golang
+
+# 安装其余所有插件
+./scripts/feeds install -a
