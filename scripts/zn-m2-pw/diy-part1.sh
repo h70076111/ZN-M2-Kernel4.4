6787295a47d89dd2a9acca00de6bfda4bb353aef
OlaveV <heifu303@gmail.com>
2026-04-05 10:34:35 +0800

    Add files via upload

--- scripts/zn-m2-pw/diy-part1.sh
+++ scripts/zn-m2-pw/diy-part1.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 移除原有的 golang feed 并替换为 26.x 版本源
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-
-# 如果需要添加其他插件源，可以在此处添加
-# echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
+# Uncomment a feed source
+#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
