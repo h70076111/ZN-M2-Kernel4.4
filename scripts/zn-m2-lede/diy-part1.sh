6be4cb8652c062b1fca22b4c92459519b4522cef
OlaveV <heifu303@gmail.com>
2026-04-13 23:24:06 +0800

    Clean up copyright and license comments in script
    Removed copyright notice and license information from the script.

--- scripts/zn-m2-lede/diy-part1.sh
+++ scripts/zn-m2-lede/diy-part1.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
+# 如需添加额外 feed，在此取消注释
+# echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
