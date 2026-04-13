dcb05b1eee7f04400b224c238b89ac91f3293fa3
OlaveV <heifu303@gmail.com>
2026-04-13 23:13:36 +0800

    Update diy-part2.sh

--- scripts/zn-m2-lede/diy-part2.sh
+++ scripts/zn-m2-lede/diy-part2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# 优先清理可能存在的同名包，防止编译中断
+# File name: diy-part2.sh
+
+# 1. 清理可能导致冲突的旧软件包，确保 OpenClash 和 Passwall 使用最新源码
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/packages/net/openclash
+rm -rf feeds/luci/applications/luci-app-passwall
 
-# 强制安装指定源的包
+# 2. 优先安装 OpenClash 相关的包
 ./scripts/feeds install -a -f -p openclash
-./scripts/feeds install -a -f -p openclash_luci
