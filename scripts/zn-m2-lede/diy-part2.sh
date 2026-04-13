bf646c318040d3273c77d617fa2ff8b13cb63d61
OlaveV <heifu303@gmail.com>
2026-04-13 23:12:02 +0800

    Update diy-part2.sh

--- scripts/zn-m2-lede/diy-part2.sh
+++ scripts/zn-m2-lede/diy-part2.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-#优先安装 openclash
+# 优先清理可能存在的同名包，防止编译中断
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/packages/net/openclash
+
+# 强制安装指定源的包
 ./scripts/feeds install -a -f -p openclash
 ./scripts/feeds install -a -f -p openclash_luci
