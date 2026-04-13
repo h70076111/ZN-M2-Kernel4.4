1308ade1e029e67d73a57b9054557b733954b2df
OlaveV <heifu303@gmail.com>
2026-04-13 23:30:53 +0800

    Update diy-part3.sh

--- scripts/zn-m2-lede/diy-part3.sh
+++ scripts/zn-m2-lede/diy-part3.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # File name: diy-part3.sh
 
-# 修改版本显示信息
+# 1. 修改版本显示信息 (ZN-M2 专用)
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt ZN-M2 (Build $(date +%Y%m%d))'/g" package/base-files/files/etc/openwrt_release
 
-# 1. 替换 Golang 1.26 (解决编译依赖问题)
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+# 2. 修改主题默认 Proxy 链接到 OpenClash
+# 适配 Design 主题的顶部导航
+if [ -f feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm ]; then
+    sed -i -r "s#navbar_proxy = 'passwall'#navbar_proxy = 'openclash'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
+fi
 
-# 2. 【关键】在换完 Golang 后强制重新同步并安装所有 feed
-./scripts/feeds update -i
-./scripts/feeds install -a -f
-
-# 3. 修改主题默认 Proxy 链接到 OpenClash
-sed -i -r "s#navbar_proxy = 'passwall'#navbar_proxy = 'openclash'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
+# 3. 修正某些插件对 golang 版本的硬编码依赖（可选）
+# 如果遇到 golang 报错，可以在此处强制同步
+# ./scripts/feeds update -i && ./scripts/feeds install -a
