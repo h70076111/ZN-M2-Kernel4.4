86cd2026d6dbb4f58c40693966d003970cd20a04
OlaveV <heifu303@gmail.com>
2026-04-13 23:29:34 +0800

    Update comments for OpenWrt DIY script

--- scripts/zn-m2-lede/diy-part1.sh
+++ scripts/zn-m2-lede/diy-part1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# 如需添加额外 feed，在此取消注释
-# echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >> feeds.conf.default
+# 确保 OpenClash 源码在 feeds 中（如果 feeds.conf.default 没写则取消下行注释）
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
