b27b9427774942bb0a03eef7cf37a76e6b9ab098
OlaveV <heifu303@gmail.com>
2026-04-14 19:49:15 +0800

    Delete scripts/zn-m2-lede directory

--- scripts/zn-m2-lede/diy-part1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-
-# 确保 OpenClash 源码在 feeds 中（如果 feeds.conf.default 没写则取消下行注释）
-# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
